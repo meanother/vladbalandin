0b6e45713d6bba9b5b9f95dcadb3af3c7986645f
arty <juicehq@yandex.ru>
2020-04-27 18:47:56 +0300

    add email field

--- config/config/settings.py
+++ config/config/settings.py
@@ -94,28 +94,28 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 #
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 #
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'OPTIONS': {
-            'options': '-c search_path=promise_rest'
-        },
-        'NAME': 'parsing_db',
-        'USER': 'semenov',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'OPTIONS': {
+#             'options': '-c search_path=promise_rest'
+#         },
+#         'NAME': 'parsing_db',
+#         'USER': 'semenov',
+#         'PASSWORD': '12345',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
--- config/onepager/models.py
+++ config/onepager/models.py
@@ -8,6 +8,7 @@ class ShortQuestions(models.Model):
     name = models.CharField(max_length=255)
     phone = models.CharField(max_length=255)
     message = models.CharField(max_length=4096)
+    email = models.EmailField(blank=True, default=None, null=True)
     is_processed = models.CharField(max_length=255, default='N')
     create_time = models.DateTimeField(auto_now_add=True)
 
@@ -22,6 +23,7 @@ class PollQuestions(models.Model):
     pledge = models.CharField(max_length=255)
     property = models.CharField(max_length=4000)
     city = models.CharField(max_length=510)
+    email = models.EmailField(blank=True, default=None, null=True)
     registration_city = models.CharField(max_length=510)
     name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=500)
